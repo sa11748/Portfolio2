1faedb8904c318d767e7d8dc725f8a8ba0c08d60
sa11748 <shubhangishubhi490@gmail.com>
2024-08-21 18:02:51 +0530

    Added some

---
+++ src/components/Contact.jsx
@@ -0,0 +1,44 @@
+
+import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { CiFacebook } from "react-icons/ci";
+import { CiLinkedin } from "react-icons/ci";
+import { FaGithubSquare } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
+const Contact = () => {
+  return (
+    <>
+      <div className="container contact" id="contact">
+        <h1>CONTACT ME</h1>
+        <div
+          className="contact-icon"
+          data-aos="zoom-in-up"
+          data-aos-duration="1000"
+        >
+          <a href="https://www.google.com" target="_blank" className="items">
+            <FaInstagram className="icons" />
+          </a>
+          <a href="https://www.google.com" target="_blank" className="items">
+            <CiFacebook className="icons" />
+          </a>
+          <a href="https://www.google.com" target="_blank" className="items">
+            <CiLinkedin className="icons" />
+          </a>
+          <a href="https://github.com/sa11748" target="_blank" className="items">
+            <FaGithubSquare className="icons" />
+          </a>
+          <a
+            href="mailto:shubhangishubhi490@gmail.com"
+            target="_blank"
+            className="items"
+          >
+            <SiGmail className="icons" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
---
+++ src/components/Home.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef } from "react";
+import pdf from "../pdf/shubhuresume.pdf";
+import hero from "./data/hero.json";
+import Typed from "typed.js";
+
+const Home = () => {
+  const typedRef = useRef(null);
+  useEffect(() => {
+    const options = {
+      strings: [
+        "Welcome to my Portfolio",
+        "My Name is Shubhangi",
+        "Full Stack Developer",
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+    };
+
+    const typed = new Typed(typedRef.current, options);
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="container home" id="home">
+        <div className="left" data-aos="fade-up-right" data-aos-duration="1000">
+          <h1 ref={typedRef}>Loading..</h1>
+
+          <a
+            href={pdf}
+            download="shubhuresume.pdf"
+            className="btn btn-outline-warning my-3"
+          >
+            Download Resume
+          </a>
+        </div>
+        <div className="right">
+          <div className="img" data-aos="fade-up-left" data-aos-duration="1000">
+            <img src={`/assets${hero.imgSrc}`} alt="hero" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
