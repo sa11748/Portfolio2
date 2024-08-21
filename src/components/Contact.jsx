98e95bd4d175211d7a883a98215c3a6bcadb9a7b
sa11748 <shubhangishubhi490@gmail.com>
2024-08-21 18:11:24 +0530

    solve one warning

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,21 +16,22 @@ const Contact = () => {
           data-aos="zoom-in-up"
           data-aos-duration="1000"
         >
-          <a href="https://www.google.com" target="_blank" className="items">
+          <a href="https://www.google.com" target="_blank" rel="noopener noreferrer" className="items">
             <FaInstagram className="icons" />
           </a>
-          <a href="https://www.google.com" target="_blank" className="items">
+          <a href="https://www.google.com" target="_blank" rel="noopener noreferrer" className="items">
             <CiFacebook className="icons" />
           </a>
-          <a href="https://www.google.com" target="_blank" className="items">
+          <a href="https://www.google.com" target="_blank"  rel="noopener noreferrer" className="items">
             <CiLinkedin className="icons" />
           </a>
-          <a href="https://github.com/sa11748" target="_blank" className="items">
+          <a href="https://github.com/sa11748" target="_blank"  rel="noopener noreferrer" className="items">
             <FaGithubSquare className="icons" />
           </a>
           <a
             href="mailto:shubhangishubhi490@gmail.com"
             target="_blank"
+            rel="noopener noreferrer"
             className="items"
           >
             <SiGmail className="icons" />
